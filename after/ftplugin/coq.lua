8846ed035e16607b3a05765bb3ae40654a1df3de
Felix Prasanna <fpx@mit.edu>
2024-12-21 16:32:35 -0500

    Remove unused options

--- after/ftplugin/coq.lua
+++ after/ftplugin/coq.lua
@@ -4,12 +4,12 @@ vim.cmd([[hi def CoqtailChecked guibg=#113311]])
 -- coq-to-line
 vim.keymap.set("n", "<C-k>l", function()
     vim.cmd([[CoqToLine]])
-end, opts)
+end)
 -- coq-down
 vim.keymap.set("n", "<Down>", function()
     vim.cmd([[CoqNext]])
-end, opts)
+end)
 -- coq-up
 vim.keymap.set("n", "<Up>", function()
     vim.cmd([[CoqUndo]])
-end, opts)
+end)
